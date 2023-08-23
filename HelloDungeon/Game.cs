bb8633e601f45737c4b620a41d37ee839947a6f4
lodisperkins <lodisperkins44@gmail.com>
2023-08-23 15:05:50 -0500

    Added player stats and completed operators exercise

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -10,7 +10,24 @@ namespace HelloDungeon
     {
         public void Run()
         {
+            //Initializes player stats.
+            string playerName;
+            float damage = 3.2f;
+            float health = 100.0f * 2;
+            int areaNumber = 0;
+            bool playerIsAlive = health > 0;
 
+            //Print out name for first exercise.
+            Console.WriteLine("Lodis");
+            Console.WriteLine("Perkins");
+
+            //Get player input for first and last character name.
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            playerName = firstName + lastName;
+
+            //Display character name for player feedback.
+            Console.WriteLine(playerName);
         }
     }
 }
