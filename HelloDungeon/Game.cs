b8888809968b45d5a34a17b56010c4c1f1a119ca
lodisperkins <lodisperkins44@gmail.com>
2023-08-25 12:01:31 -0500

    Added weapon choice example.

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -12,27 +12,71 @@ namespace HelloDungeon
         {
             //Initializes player stats.
             string playerName;
+            string playerChoice;
             float damage = 3.2f;
             float health = 100.0f * 2;
             int areaNumber = 0;
             bool playerIsAlive = health > 0;
 
-            //Print out name for first exercise.
-            Console.WriteLine("Lodis");
-            Console.WriteLine("Perkins");
+            //Initialize weapon stats.
+            float swordDamage = 10.0f;
+            float boardDamage = 1.0f;
+            float bowThatShootsPipesDamage = 800.0f;
+
+            Console.WriteLine("What is your name.");
 
             //Get player input for first and last character name.
-            string firstName = Console.ReadLine();
-            string lastName = Console.ReadLine();
-            playerName = firstName + lastName;
+            playerName = Console.ReadLine();
 
             //Display character name for player feedback.
-            Console.WriteLine(playerName);
+            Console.WriteLine("Hello" + playerName);
+            Console.WriteLine("Choose your weapon!");
+            Console.WriteLine("1. Sword of the Ancients");
+            Console.WriteLine("2. Wooden Board of the Home Depot");
+            Console.WriteLine("3. Bow and Pipe");
+
+            playerChoice = Console.ReadLine();
+
+            //Set damage stat based on weapon selection.
+            if (playerChoice == "1")
+            {
+                damage = swordDamage;
+            }
+            else if (playerChoice == "2")
+            {
+                damage = boardDamage;
+            }
+            else if (playerChoice == "3")
+            {
+                damage = bowThatShootsPipesDamage;
+            }
+
+            float enemyHealth = 100.0f;
+            enemyHealth -= damage;
+
+            Console.WriteLine("Damage is now " + damage);
+
+
+            //Pauses the game.
+            Console.WriteLine("Press any key to continue.");
+            char character = Console.ReadKey(true).KeyChar;
+            Console.WriteLine(character);
+
 
             //Display context for decision.
             Console.WriteLine("You run into a mysterious old man on your journey.");
             Console.WriteLine("He offers you a weird bottle of kinda stinky liquid");
             Console.WriteLine("You take it in your hand and you feel that the bottle is sticky as well.");
+
+            //Symbol to show the player we expect input
+            Console.Write("> ");
+
+            //Pauses the game.
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+
+            //Clears all text on the screen.
+            Console.Clear();
 
             Console.WriteLine("'If you you you ingest this great elixir you will find yourself");
             Console.WriteLine("with more power than you could imagine.'");
@@ -46,25 +90,35 @@ namespace HelloDungeon
             Console.WriteLine("3.Yeet it in his face");
 
             //Take in player's input.
-            string playerChoice = Console.ReadLine();
+            playerChoice = Console.ReadLine();
+
+            bool enemyAlive = true;
+
+            if (playerIsAlive && enemyAlive)
+            {
+                //Perform battle logic
+            }
+            else
+            {
+                //Display battle results
+            }
+
 
             //Display text based on players decision.
-            if (playerChoice == "1")
+            if (playerChoice == "1" || playerChoice == "Yes")
             {
                 Console.WriteLine("The weird drink is oddly thick, but tastes like apples?");
                 Console.WriteLine("You notice your skin starts to harden. You gain 1200 health.");
 
                 health += 1200;
             }
-            
-            if (playerChoice == "2")
+            else if (playerChoice == "2" || playerChoice == "No")
             {
                 Console.WriteLine("You go with your gut feeling and decline the old man's offer.");
                 Console.WriteLine("The old man is offended at first but shrugs his shoulders in understand.");
                 Console.WriteLine("You gain 8 wisdom. You lose 5 intelligence.");
             }
-
-            if (playerChoice == "3")
+            else if (playerChoice == "3" || playerChoice == "Yeet it in his face")
             {
                 Console.WriteLine("The old man dodges at point blank range.");
                 Console.WriteLine("He is not angry. He is just disappointed.");
@@ -73,6 +127,12 @@ namespace HelloDungeon
 
                 health -= 5;
             }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+
         }
     }
 }
